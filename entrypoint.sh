4f8dfcdab7b362bf82d2e44e1d9ff9a20c192ed0
Asim Aslam <asim@aslam.me>
2020-02-14 21:26:14 +0000

    Add platform-test readme

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,7 +7,7 @@ SOURCE=$1
 REPO=github.com/micro/services
 
 # clone the repo
-echo "Downloading $SOURCE"
+echo "Cloning $REPO"
 git clone --no-checkout https://$REPO
 
 # cd into source
@@ -17,10 +17,16 @@ cd services
 git sparse-checkout init --cone
 
 # set the repo to checkout
+echo "Checking out $SOURCE"
 git sparse-checkout set $SOURCE
 
 # go to source
 cd $SOURCE
 
+# download deps
+echo "Downloading deps"
+go get -d
+
 # run the source
+echo "Running service"
 go run .
