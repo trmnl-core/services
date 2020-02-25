ae945b83c4ad0a8858af242983004ab965fc6294
Asim Aslam <asim@aslam.me>
2020-02-25 10:38:23 +0000

    add github run id to events (#27)

--- event.sh
+++ event.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 
+# event is build.{started, failed, finished}
 EVENT=$1
-COMMIT=$2
-REPO=$3
+# build is the build number to be used at $REPO/actions/runs/$BUILD
+BUILD=$2
+# commit is a git hash to be used as $REPO/commit/$COMMIT
+COMMIT=$3
+# repo is a full github.com/micro/services url
+REPO=$4
+# the url to send events to
 URL=https://micro.mu/platform/v1/github/events
 
 curl $URL -X POST -d @$HOME/services.json \
 -H "Content-Type: application/json" \
 -H "Micro-Event: $EVENT" \
--H "X-Github-Sha: $COMMIT" \
+-H "X-Github-Build: $BUILD" \
+-H "X-Github-Commit: $COMMIT" \
 -H "X-Github-Repo: $REPO"
