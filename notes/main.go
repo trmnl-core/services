0a6816d47e746fe5a5a25f0506e3f4e3cb54a666
Ben Toogood <ben@micro.mu>
2020-02-27 19:58:16 +0000

    Add notes top level service

---
+++ notes/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"notes/handler"
+
+	"github.com/micro/go-micro/v2"
+	log "github.com/micro/go-micro/v2/logger"
+
+	notes "notes/proto/notes"
+)
+
+func main() {
+	// New Service
+	service := micro.NewService(
+		micro.Name("go.micro.srv.notes"),
+		micro.Version("latest"),
+	)
+
+	// Initialise service
+	service.Init()
+
+	// Register Handler
+	notes.RegisterNotesHandler(service.Server(), handler.NewHandler())
+
+	// Run service
+	if err := service.Run(); err != nil {
+		log.Fatal(err)
+	}
+}
