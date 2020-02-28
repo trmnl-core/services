e6b708f301bbfa2318ec29de912ac1a33202ff69
Vasiliy Tolstov <v.tolstov@unistack.org>
2020-02-28 22:42:25 +0300

    Initialising service vtolstov

---
+++ vtolstov/go.mod
@@ -0,0 +1,5 @@
+module vtolstov
+
+go 1.14
+
+require github.com/micro/go-micro/v2 v2.2.0
---
+++ vtolstov/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"github.com/micro/go-micro/v2/util/log"
+	"github.com/micro/go-micro/v2"
+	"vtolstov/handler"
+	"vtolstov/subscriber"
+
+	vtolstov "vtolstov/proto/vtolstov"
+)
+
+func main() {
+	// New Service
+	service := micro.NewService(
+		micro.Name("go.micro.srv.vtolstov"),
+		micro.Version("latest"),
+	)
+
+	// Initialise service
+	service.Init()
+
+	// Register Handler
+	vtolstov.RegisterVtolstovHandler(service.Server(), new(handler.Vtolstov))
+
+	// Register Struct as Subscriber
+	micro.RegisterSubscriber("go.micro.srv.vtolstov", service.Server(), new(subscriber.Vtolstov))
+
+	// Run service
+	if err := service.Run(); err != nil {
+		log.Fatal(err)
+	}
+}
