df289fceb4f935a70fd3d1b920af3f0dbdcb98f2
Sumanth Chinthagunta <xmlking@gmail.com>
2020-02-28 12:30:16 -0800

    Initialising service sumo

---
+++ sumo/go.mod
@@ -0,0 +1,8 @@
+module sumo
+
+go 1.14
+
+require (
+	github.com/golang/protobuf v1.3.4
+	github.com/micro/go-micro/v2 v2.2.0
+)
---
+++ sumo/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	log "github.com/micro/go-micro/v2/logger"
+	"github.com/micro/go-micro/v2"
+	"sumo/handler"
+	"sumo/subscriber"
+
+	sumo "sumo/proto/sumo"
+)
+
+func main() {
+	// New Service
+	service := micro.NewService(
+		micro.Name("go.micro.srv.sumo"),
+		micro.Version("latest"),
+	)
+
+	// Initialise service
+	service.Init()
+
+	// Register Handler
+	sumo.RegisterSumoHandler(service.Server(), new(handler.Sumo))
+
+	// Register Struct as Subscriber
+	micro.RegisterSubscriber("go.micro.srv.sumo", service.Server(), new(subscriber.Sumo))
+
+	// Run service
+	if err := service.Run(); err != nil {
+		log.Fatal(err)
+	}
+}
