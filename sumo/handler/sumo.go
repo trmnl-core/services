65fced56e15d6b6818043fe866fa608afa46d1d9
Sumanth Chinthagunta <xmlking@gmail.com>
2020-02-28 13:46:58 -0800

    second commit

--- sumo/handler/sumo.go
+++ sumo/handler/sumo.go
@@ -12,14 +12,14 @@ type Sumo struct{}
 
 // Call is a single request handler called via client.Call or the generated client code
 func (e *Sumo) Call(ctx context.Context, req *sumo.Request, rsp *sumo.Response) error {
-	log.Info("Received Sumo.Call request")
-	rsp.Msg = "Hello " + req.Name
+	log.Info("Received v2 Sumo.Call request")
+	rsp.Msg = "Hello U:" + req.Name
 	return nil
 }
 
 // Stream is a server side stream handler called via client.Stream or the generated client code
 func (e *Sumo) Stream(ctx context.Context, req *sumo.StreamingRequest, stream sumo.Sumo_StreamStream) error {
-	log.Infof("Received Sumo.Stream request with count: %d", req.Count)
+	log.Infof("Received v2 Sumo.Stream request with count: %d", req.Count)
 
 	for i := 0; i < int(req.Count); i++ {
 		log.Infof("Responding: %d", i)
