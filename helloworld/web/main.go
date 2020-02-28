72aaef24eb7ee82491f64ecf982cbc63db8a4bff
Asim Aslam <asim@aslam.me>
2020-02-28 23:42:05 +0000

    update helloworld webapp

--- helloworld/web/main.go
+++ helloworld/web/main.go
@@ -10,6 +10,11 @@ import (
 	hello "github.com/micro/services/helloworld/proto/helloworld"
 )
 
+var (
+	head = `<head><style>body {margin: 25px; font-family: sans-serif;}</style></head>`
+	html = `<html>` + head + `<body><h1>Enter Name<h1><form method=post><input name=name type=text /></form></body></html>`
+)
+
 func main() {
 	service := web.NewService(
 		web.Name("go.micro.web.helloworld"),
@@ -36,11 +41,11 @@ func main() {
 				return
 			}
 
-			w.Write([]byte(`<html><body><h1>` + rsp.Msg + `</h1></body></html>`))
+			w.Write([]byte(`<html>` + head + `<body><h1>` + rsp.Msg + `</h1></body></html>`))
 			return
 		}
 
-		fmt.Fprint(w, `<html><body><h1>Enter Name<h1><form method=post><input name=name type=text /></form></body></html>`)
+		fmt.Fprint(w, html)
 	})
 
 	if err := service.Init(); err != nil {
