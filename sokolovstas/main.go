f97f01964dfd90eea54fe21e23ddf460014e5e1d
Stanislav Sokolov <sokolov.stas@gmail.com>
2020-03-04 15:45:04 +0500

    Initialising service sokolovstas

---
+++ sokolovstas/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	log "github.com/micro/go-micro/v2/logger"
+	"github.com/micro/go-micro/v2"
+	"sokolovstas/handler"
+	"sokolovstas/subscriber"
+
+	sokolovstas "sokolovstas/proto/sokolovstas"
+)
+
+func main() {
+	// New Service
+	service := micro.NewService(
+		micro.Name("go.micro.srv.sokolovstas"),
+		micro.Version("latest"),
+	)
+
+	// Initialise service
+	service.Init()
+
+	// Register Handler
+	sokolovstas.RegisterSokolovstasHandler(service.Server(), new(handler.Sokolovstas))
+
+	// Register Struct as Subscriber
+	micro.RegisterSubscriber("go.micro.srv.sokolovstas", service.Server(), new(subscriber.Sokolovstas))
+
+	// Run service
+	if err := service.Run(); err != nil {
+		log.Fatal(err)
+	}
+}
