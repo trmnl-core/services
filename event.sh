e9fc677d0c00e665fe503443f9ccec4ec1604e82
Asim Aslam <asim@aslam.me>
2020-03-16 15:29:59 +0000

    change event url (#60)

--- event.sh
+++ event.sh
@@ -5,7 +5,7 @@ EVENT=$1
 # build is the build number to be used at $REPO/actions/runs/$BUILD
 BUILD=$2
 # the url to send events to
-URL=https://micro.mu/platform/v1/github/events
+URL=https://web.micro.mu/platform/v1/github/events
 
 curl $URL -X POST -d @$HOME/changes.json \
 -H "Content-Type: application/json" \
