cd9c440d06c0e713da15774038cc5fd1f34cb0b2
Ben Toogood <ben@micro.mu>
2020-04-01 15:01:26 +0100

    Revert Debugging

--- account/web/handler/github.go
+++ account/web/handler/github.go
@@ -47,7 +47,6 @@ func (h *Handler) HandleGithubOauthVerify(w http.ResponseWriter, req *http.Reque
 		Token string `json:"access_token"`
 	}
 	json.NewDecoder(resp.Body).Decode(&result)
-	fmt.Println(result.Token)
 
 	// Use the token to get the users profile
 	r, _ = http.NewRequest("GET", "https://api.github.com/user", nil)
