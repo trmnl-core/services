d48030b70eb5f2cf333b11b1d68e657bc0ae34b8
ben-toogood <bentoogood@gmail.com>
2020-04-02 14:33:06 +0100

    Fix Onboarding Bug (Missing Payment Methods etc) (#151)
    Co-authored-by: Ben Toogood <ben@micro.mu>

--- account/api/handler/auth.go
+++ account/api/handler/auth.go
@@ -5,9 +5,11 @@ import (
 	"time"
 
 	"github.com/micro/go-micro/v2/client"
+	log "github.com/micro/go-micro/v2/logger"
 
 	"github.com/micro/go-micro/v2/auth"
 	pb "github.com/micro/services/account/api/proto/account"
+	payment "github.com/micro/services/payments/provider/proto"
 	users "github.com/micro/services/users/service/proto"
 )
 
@@ -24,7 +26,7 @@ func (h *Handler) Token(ctx context.Context, req *pb.TokenRequest, rsp *pb.Token
 // Login looks up an account using an email and password
 func (h *Handler) Login(ctx context.Context, req *pb.LoginRequest, rsp *pb.LoginResponse) error {
 	// Generate a token
-	tok, err := h.auth.Token(auth.WithCredentials(req.Email, req.Password))
+	tok, err := h.auth.Token(auth.WithCredentials(req.Email, req.Password), auth.WithExpiry(time.Hour*24))
 	if err != nil {
 		return err
 	}
@@ -38,6 +40,33 @@ func (h *Handler) Login(ctx context.Context, req *pb.LoginRequest, rsp *pb.Login
 	// Serialize the response
 	rsp.User = serializeUser(uRsp.User)
 	rsp.Token = serializeToken(tok)
+
+	// Fetch the payment methods
+	pRsp, err := h.payment.ListPaymentMethods(ctx, &payment.ListPaymentMethodsRequest{UserId: uRsp.User.Id})
+	if err != nil {
+		log.Infof("Error listing payment methods: %v", err)
+		return nil
+	}
+
+	// Serialize the payment methods
+	rsp.User.PaymentMethods = make([]*pb.PaymentMethod, len(pRsp.PaymentMethods))
+	for i, p := range pRsp.PaymentMethods {
+		rsp.User.PaymentMethods[i] = serializePaymentMethod(p)
+	}
+
+	// Fetch the subscriptions
+	sRsp, err := h.payment.ListSubscriptions(ctx, &payment.ListSubscriptionsRequest{UserId: uRsp.User.Id})
+	if err != nil {
+		log.Infof("Error listing subscriptions: %v", err)
+		return nil
+	}
+
+	// Serialize the subscriptions
+	rsp.User.Subscriptions = make([]*pb.Subscription, len(sRsp.Subscriptions))
+	for i, s := range sRsp.Subscriptions {
+		rsp.User.Subscriptions[i] = serializeSubscription(s)
+	}
+
 	return nil
 }
 
