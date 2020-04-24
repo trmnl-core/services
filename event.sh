8693b5da020af64f02661c354bb9a6acf0e7ed35
Asim Aslam <asim@aslam.me>
2020-04-24 13:31:17 +0100

    send events to staging (#189)

--- event.sh
+++ event.sh
@@ -5,10 +5,23 @@ EVENT=$1
 # build is the build number to be used at $REPO/actions/runs/$BUILD
 BUILD=$2
 # the url to send events to
-URL=https://web.micro.mu/platform/v1/github/events
+LIVE=https://web.micro.mu/platform/v1/github/events
+# the staging url to send events to
+STAGING=https://web.m3o.dev/platform/v1/github/events
 
-curl --connect-timeout 5 --retry 3 -s -S \
-$URL -X POST -d @$HOME/changes.json \
--H "Content-Type: application/json" \
--H "X-Github-Build: $BUILD" \
--H "Micro-Event: $EVENT"
+sendEvent() {
+	URL=$1
+
+	echo "sending event $EVENT to $URL"
+	curl --connect-timeout 5 --retry 3 -s -S \
+	$URL -X POST -d @$HOME/changes.json \
+	-H "Content-Type: application/json" \
+	-H "X-Github-Build: $BUILD" \
+	-H "Micro-Event: $EVENT"
+}
+
+## send event to live
+## TODO: only on releases
+sendEvent $LIVE
+## sent event to staging
+sendEvent $STAGING
