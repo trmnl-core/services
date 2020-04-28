ea96e5be31b696aeddf5f0f2bc5236560d82a6e6
Asim Aslam <asim@aslam.me>
2020-04-28 22:03:10 +0100

    fix users api

--- users/api/handler/handler.go
+++ users/api/handler/handler.go
@@ -25,9 +25,9 @@ func NewHandler(srv micro.Service) *Handler {
 // Read retrieves a user from the users service
 func (h *Handler) Read(ctx context.Context, req *pb.ReadRequest, rsp *pb.ReadResponse) error {
 	// Identify the user
-	acc, err := auth.AccountFromContext(ctx)
-	if err != nil {
-		return err
+	acc, ok := auth.AccountFromContext(ctx)
+	if !ok {
+		return errors.Unauthorized("go.micro.api.users", "account not found")
 	}
 	if acc == nil {
 		return errors.Unauthorized("go.micro.api.users", "A valid auth token is required")
@@ -47,9 +47,9 @@ func (h *Handler) Read(ctx context.Context, req *pb.ReadRequest, rsp *pb.ReadRes
 // Update modifies a user in the users service
 func (h *Handler) Update(ctx context.Context, req *pb.UpdateRequest, rsp *pb.UpdateResponse) error {
 	// Identify the user
-	acc, err := auth.AccountFromContext(ctx)
-	if err != nil {
-		return err
+	acc, ok := auth.AccountFromContext(ctx)
+	if !ok {
+		return errors.Unauthorized("go.micro.api.users", "account not found")
 	}
 	if acc == nil {
 		return errors.Unauthorized("go.micro.api.users", "A valid auth token is required")
@@ -75,16 +75,16 @@ func (h *Handler) Update(ctx context.Context, req *pb.UpdateRequest, rsp *pb.Upd
 // Delete a user in the store
 func (h *Handler) Delete(ctx context.Context, req *pb.DeleteRequest, rsp *pb.DeleteResponse) error {
 	// Identify the user
-	acc, err := auth.AccountFromContext(ctx)
-	if err != nil {
-		return err
+	acc, ok := auth.AccountFromContext(ctx)
+	if !ok {
+		return errors.Unauthorized("go.micro.api.users", "account not found")
 	}
 	if acc == nil {
 		return errors.Unauthorized("go.micro.api.users", "A valid auth token is required")
 	}
 
 	// Delete the user
-	_, err = h.users.Delete(ctx, &users.DeleteRequest{Id: acc.ID})
+	_, err := h.users.Delete(ctx, &users.DeleteRequest{Id: acc.ID})
 	return err
 }
 
