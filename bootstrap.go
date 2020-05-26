c912c7e17497d62d45027b4f44fb9ba11d26cee7
Ben Toogood <bentoogood@gmail.com>
2020-05-26 15:00:40 +0100

    Bootstrap in platform namespace

--- bootstrap.go
+++ bootstrap.go
@@ -1,11 +1,13 @@
 package main
 
 import (
+	"fmt"
 	"os"
 	"path/filepath"
 	"strings"
 
 	"github.com/micro/go-micro/v2"
+	"github.com/micro/go-micro/v2/auth"
 	"github.com/micro/go-micro/v2/logger"
 	"github.com/micro/go-micro/v2/runtime"
 )
@@ -18,9 +20,43 @@ const (
 )
 
 func main() {
+	srv := micro.NewService()
+	srv.Init()
+	logger.Infof("Using %v runtime", srv.Options().Runtime)
+
+	// setup an admin account for the service to use (required to run services in a custom namespace)
+	// this is a temporaty solution until identity is setup then we'll need to pass a set of creds
+	// as arguments.
+	name := fmt.Sprintf("bootstrap-%v", srv.Options().Server.Options().Id)
+	acc, err := srv.Options().Auth.Generate(name, auth.WithRoles("admin"))
+	if err != nil {
+		logger.Fatal(err)
+	}
+	tok, err := srv.Options().Auth.Token(auth.WithCredentials(acc.ID, acc.Secret))
+	if err != nil {
+		logger.Fatal(err)
+	}
+	srv.Options().Auth.Init(auth.ClientToken(tok))
+
+	for _, name := range listServices() {
+		logger.Infof("Creating %v", name)
+
+		err := srv.Options().Runtime.Create(
+			&runtime.Service{Name: name, Source: source},
+			runtime.CreateNamespace("platform"),
+			runtime.CreateImage("docker.pkg.github.com/micro/services/"+strings.ReplaceAll(name, "/", "-")),
+		)
+
+		if err != nil {
+			logger.Fatal(err)
+		}
+	}
+}
+
+func listServices() []string {
 	var services []string
 
-	walker := func(path string, info os.FileInfo, err error) error {
+	filepath.Walk(".", func(path string, info os.FileInfo, err error) error {
 		if err != nil {
 			return err
 		}
@@ -29,27 +65,7 @@ func main() {
 		}
 		services = append(services, filepath.Dir(path))
 		return nil
-	}
+	})
 
-	if err := filepath.Walk(".", walker); err != nil {
-		logger.Fatal(err)
-	}
-
-	srv := micro.NewService()
-	srv.Init()
-	logger.Infof("Using %v runtime", srv.Options().Runtime)
-
-	for _, name := range services {
-		logger.Infof("Creating %v", name)
-
-		err := srv.Options().Runtime.Create(
-			&runtime.Service{Name: name, Source: source},
-			runtime.CreateNamespace("micro"),
-			runtime.CreateImage("docker.pkg.github.com/micro/services/"+strings.ReplaceAll(name, "/", "-")),
-		)
-
-		if err != nil {
-			logger.Fatal(err)
-		}
-	}
+	return services
 }
