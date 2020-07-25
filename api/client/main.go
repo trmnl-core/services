ec58c61e1605aae91c72dec80bc823a096429a4a
Janos Dobronszki <dobronszki@gmail.com>
2020-07-26 00:11:21 +0200

    [WIP] Moving api back from microhq (#281)
    * Moving api back from microhq
    
    * Rename and remove go.mod && go.sum
    
    * Path fixes
    
    * Move
    
    * move to api/client
    
    * Fix import path
    
    Co-authored-by: Asim Aslam <asim@aslam.me>

---
+++ api/client/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"github.com/micro/services/api/client/handler"
+	client "github.com/micro/services/api/client/proto/client"
+
+	"github.com/micro/go-micro/v2"
+	"github.com/micro/go-micro/v2/api"
+	log "github.com/micro/go-micro/v2/logger"
+)
+
+func main() {
+	// New Service
+	service := micro.NewService(
+		micro.Name("go.micro.api.client"),
+	)
+
+	// Initialise service
+	service.Init()
+
+	// Register Handler
+	client.RegisterClientHandler(service.Server(), &handler.Client{service.Client()}, api.WithEndpoint(
+		// TODO: remove when api supports Call method as default for /foo singular paths
+		&api.Endpoint{
+			Name:    "Client.Call",
+			Path:    []string{"^/client?$"},
+			Method:  []string{"GET", "POST"},
+			Handler: "rpc",
+		},
+	))
+
+	// Run service
+	if err := service.Run(); err != nil {
+		log.Fatal(err)
+	}
+}
